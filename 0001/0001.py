92e6406259d87bcceab77b5c5301551574195353
PengQianheng <598507680@qq.com>
2016-07-07 11:59:20 +0800

    添加第0001题

---
+++ 0001/0001.py
@@ -0,0 +1,19 @@
+﻿import random,os
+
+#生成随机数
+def Activation_Code():  
+    code = ''
+    for i in range(0,10):
+        randnum = random.randint(0,9)
+        code += str(randnum)
+    code += ' '    
+    return code
+
+#保存结果
+def result_save():
+    result = open("result.txt",'ab')
+    result.write(Activation_Code().encode()) #Python3中此处需使用encode方法
+    result.close()
+
+for i in range(0,200):
+    result_save()
